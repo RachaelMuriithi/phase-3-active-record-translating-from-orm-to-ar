45c4f6285e002f6fe562ffddf51f3f87298a66e5
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-11-03 23:43:40 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,22 @@
-class Dog
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dogs.sqlite"
+)
+
+# Create a dogs table
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
 end
